71112553cf6d8cafd1e045b071c1df7bd1e27869
Richard Wood <rwoodnz@gmail.com>
2017-03-18 15:13:47 +1300

    folder tidy up

---
+++ src/app/JobsSignUp/jobs-signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'JobsSignup',
+  templateUrl: 'jobs-signup.component.html',
+  styleUrls: ['']
+})
+
+
+export class JobsSignupComponent {}
