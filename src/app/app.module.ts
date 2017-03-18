82d487e5246e3c93831a8b0d347171d84b551cc3
Ashley Stewart <Ashley Stewart>
2017-03-18 19:08:33 +1300

    cors needed, query working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { VolunteerSignupComponent } from './VolunteerSignup/volunteer-signup.component';
 import { JobSignupComponent } from './JobSignup/job-signup.component'
 import { OrganisationSignupComponent } from './OrganisationSignup/organisation-signup.component'
+import { VolunteerQueryComponent } from './Volunteer-query/volunteer-query.component'
 
 @NgModule({
   imports: [BrowserModule,
-    FormsModule],
+    FormsModule,
+    HttpModule],
 
   declarations: [AppComponent,
-    VolunteerSignupComponent, JobSignupComponent, OrganisationSignupComponent],
+    VolunteerSignupComponent, JobSignupComponent, OrganisationSignupComponent, VolunteerQueryComponent],
 
   bootstrap: [AppComponent]
 })
