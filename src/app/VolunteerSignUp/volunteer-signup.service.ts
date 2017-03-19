21828c9e6c46d31f443230b822abb6f8584cf3bb
Ashley Stewart <Ashley Stewart>
2017-03-19 13:23:50 +1300

    getting rid of double up folder

--- src/app/VolunteerSignUp/volunteer-signup.service.ts
+++ src/app/VolunteerSignUp/volunteer-signup.service.ts
@@ -30,7 +30,7 @@ console.log(volunteer);
 
         this.headers.append('Content-Type', 'application/json');
         this.http.put(this.url, JSON.stringify(volunteer), {headers: this.headers})
-        .map(res => res.json());         
+        // .map(res => res.json());         
             
         }   
 
