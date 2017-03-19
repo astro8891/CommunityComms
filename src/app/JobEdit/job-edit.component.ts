57fb6a6286a79679e82b1dd96f12e60027d891ce
Dominic Taylor <dataylorx@gmail.com>
2017-03-19 14:18:59 +1300

    Add job edit view

--- src/app/JobEdit/job-edit.component.ts
+++ src/app/JobEdit/job-edit.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 import { Job } from './job'
 import { JobEditService } from './job-edit.service'
 
+@Component({
+  moduleId: module.id,
+  selector: 'JobEdit',
+  templateUrl: 'job-edit.component.html',
+  styleUrls: ['']
+})
 
 export class JobEditComponent {
-    
+
 }
--- src/app/JobEdit/job-edit.service.ts
+++ src/app/JobEdit/job-edit.service.ts
@@ -1,8 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http, HttpModule, RequestOptions, Headers } from '@angular/http'
 
+import 'rxjs/add/operator/toPromise'
 
+import { job } from '../JobEdit/job';
 
-
+@Injectable()
 
 export class JobEditService {
-    
+
 }
